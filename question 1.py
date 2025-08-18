3dd6571dc185dc75b08fe30743cbe489a6d6c855
Y2kCRDA <maruwonde19@gmail.com>
2025-08-18 21:43:34 +0300

    Calculeter.py

--- question 1.py
+++ question 1.py
@@ -1,71 +1,85 @@
+import math
 
+def calculator():
+    print("Standard Calculator")
+    try:
+        while True:  
+            op = input("Enter operation (+, -, *, /, sin, cos, tan, sqrt, exit): ").lower()
+            
+            if op == "exit":
+                print("Exiting the calculator...")
+                break
 
-TICK = "✅"
-CROSS = "❌"
+            list_op = ["+", "-", "*", "/", "sin", "cos", "tan", "sqrt"]
+            if op not in list_op:
+                print("Please enter a valid operator!")
+                continue
 
+            numbers = []
+            while True:
+                number = input("Enter number (or = to calculate): ")
+                if number == "=":
+                    if len(numbers) < 1:
+                        print("Enter at least 1 number.")
+                        continue
+                    break
+                try:
+                    num = float(number)
+                    numbers.append(num)
+                except ValueError:
+                    print("Enter numbers only!")
+                    continue
+            if op == "+":
+                result = numbers[0]
+                for number in numbers[1:]:
+                    result += number
+                print(" + ".join(map(str, numbers)), "=", result)
 
-questions = [
-    {
-        "question": "English: What is the synonym of 'happy'?",
-        "choices": ["Sad", "Angry", "Joyful", "Tired"],
-        "correct_index": 2  
-    },
-    {
-        "question": "English: What is the past tense of 'eat'?",
-        "choices": ["Eated", "Ate", "Eats", "Eating"],
-        "correct_index": 1  
-    },
-    {
-        "question": "Math: What is 7 + 5?",
-        "choices": ["12", "10", "14", "11"],
-        "correct_index": 0  
-    },
-    {
-        "question": "Math: What is 9 × 3?",
-        "choices": ["27", "18", "21", "36"],
-        "correct_index": 0  
-    }
-]
+            elif op == "-":
+                result = numbers[0]
+                for number in numbers[1:]:
+                    result -= number
+                print(" - ".join(map(str, numbers)), "=", result)
 
+            elif op == "*":
+                result = numbers[0]
+                for number in numbers[1:]:
+                    result *= number
+                print(" * ".join(map(str, numbers)), "=", result)
 
-letter_map = ["A", "B", "C", "D"]
+            elif op == "/":
+                result = numbers[0]
+                try:
+                    for number in numbers[1:]:
+                        result /= number
+                    print(" / ".join(map(str, numbers)), "=", result)
+                except ZeroDivisionError:
+                    print("Error: Division by zero")
+            elif op == "sin":
+                for number in numbers:
+                    print(f"sin({number}) = {math.sin(math.radians(number))}")
 
+            elif op == "cos":
+                for number in numbers:
+                    print(f"cos({number}) = {math.cos(math.radians(number))}")
 
-score = 0
+            elif op == "tan":
+                for number in numbers:
+                    try:
+                        print(f"tan({number}) = {math.tan(math.radians(number))}")
+                    except Exception as e:
+                        print(f"Error: {e}")
+            elif op == "sqrt":
+                for number in numbers:
+                    if number < 0:
+                        print(f"sqrt({number}) = Invalid (negative number)")
+                    else:
+                        print(f"√{number} = {math.sqrt(number)}")
 
-letter_map = ["A", "B", "C", "D"]
+    except Exception as e:
+        print(f"Invalid operation: {e}")
 
+        print("Developed by: Student")
 
-score = 0
+calculator()
 
-print(" Multiple Choice Quiz - English & Math")
-print("-----------------------------------------")
-
-
-for i, q in enumerate(questions, 1):
-    print(f"nQ{i}: {q['question']}")
-    for idx, choice in enumerate(q["choices"]):
-        print(f"   {letter_map[idx]}. {choice}")
-    
-  
-    user_input = input("Your answer (A, B, C, D): ")
-    if user_input in letter_map:
-        user_index = letter_map.index(user_input)
-        if user_index == q["correct_index"]:
-            print(f"{TICK} Correct!\n")
-            score += 1
-        else:
-            correct_letter = letter_map[q["correct_index"]]
-            correct_answer = q["choices"][q["correct_index"]]
-            print(f"{CROSS} Wrong! Correct answer is: {correct_letter}. {correct_answer}\n")
-    else:
-        print(f"{CROSS} Invalid choice. Skipped.\n")
-
-print("-----------------------------------------")
-print(f" You scored {score} out of {len(questions)}.")
-if score == len(questions):
-    print(" Perfect! Excellent job!")
-elif score >= len(questions) // 2:
-    print("👍 Good job! Keep practicing.")
-else:
-    print("Try again. You’ll get better!")
